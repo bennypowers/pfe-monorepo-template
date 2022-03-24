281caea50f41226e518829c7e4c315e54d3b9130
Benny Powers <web@bennypowers.com>
2022-03-24 09:39:17 -0400

    chore: fix test runner config

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,10 +1,3 @@
 import { pfeTestRunnerConfig } from '@patternfly/pfe-tools/test-runner.js';
-import { resolvePFEMonorepoPlugin, fakePrismModule } from './web-dev-server.config.js';
 
-export default pfeTestRunnerConfig({
-  files: ['!tools/create-element/templates/**/*'],
-  plugins: [
-    resolvePFEMonorepoPlugin(),
-    fakePrismModule(),
-  ],
-});
+export default pfeTestRunnerConfig();
